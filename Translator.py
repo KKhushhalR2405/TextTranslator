b3483f678a8d0d3cf31bb57128159c8a93cba1c3
KKhushhalR2405 <reddykhushhal2405@gmail.com>
2020-05-13 21:41:34 +0530

    Python file

---
+++ Translator.py
@@ -0,0 +1,24 @@
+from googletrans import Translator
+import cv2
+import pytesseract
+
+def trans(word):
+    translator=Translator(service_urls=['translate.google.com'])
+    translation1=translator.translate(word,dest="hi")
+    return translation1.text
+
+
+custom_config = r'--oem 3 --psm 6'
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
+
+
+####--------Main--------####
+
+img = cv2.imread("img2.jpg")
+text=pytesseract.image_to_string(img)
+print(text)
+print(trans(text))
+
+
+
